df502a257992e48be046998f4d396cea0fd602d4
deluxturtle <sebaturtle@gmail.com>
2021-07-01 16:34:04 -0500

    racking my brain around orbits :p

--- main.c
+++ main.c
@@ -1,17 +1,26 @@
 #include <gb/gb.h>
 
-//1,000 Km = 1 Mm
-const uint16_t kerbin_r = 600;//Mm radius of kerbin
-const uint8_t gravity = 10;//roughly close to 9.8 haha
 
-struct ship{
+
+struct Ship{
     //meters per second
     //just use this to count to 1,000
     uint16_t mspeed;
     //kilometers per second...Gets updated every 1,000 meters. 
-    //fastest ship went 150 km/s
+    //fastest irl craft went 150 km/s
     uint8_t kspeed;
+    //##.## two decimal places
+    uint8_t mass;//tons
 };
+
+struct Planet{
+    uint8_t gravity;
+    //1,000 Km = 1 Mm
+    uint16_t radius;//Mm radius
+};
+
+struct Ship player;
+struct Planet kerbin;
 
 void main(){
     // remaps the palette
@@ -26,10 +35,13 @@ void main(){
     //           3 1 0 transparent
     OBP0_REG = 0b11010000;
 
-    struct ship player;
+    player.mass = 1;//1 ton? = 907kg?
     player.mspeed = 200;//speed of ksp orbit ships.
     player.kspeed = 2;
     //now speed is 2,200 m/s
+
+    kerbin.gravity = 10;
+    kerbin.radius = 600;
 
     SHOW_SPRITES;
     while(1){
